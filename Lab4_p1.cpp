cd0e8ef5bc5a54664590a6eadc0e0f9592511afb
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2022-09-01 09:04:07 -0600

    gitignore

---
+++ Lab4_p1.cpp
@@ -0,0 +1,27 @@
+//Universidad del Valle de Guatemala
+//Programacion de Microprocesadores
+//Seccion 21
+//Catedratico Roger Diaz
+//Segundo Semestre 2022
+//Jose Daniel Gomez Cabrera 21429
+//Actividad: Laboratorio 4 Parte 1
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+//VARIABLES
+
+
+//-----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
+//SUB-RUTINE
+//-----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
+
+
